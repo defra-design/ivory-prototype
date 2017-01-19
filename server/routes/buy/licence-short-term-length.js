71527ae22918cd9257284c30496ee09e50123b48
Cathy <Cathy@Steves-MacBook-Pro-2.local>
2017-01-19 14:54:18 +0000

    Commit
    Add message at 12-month selection if dates are not compatable - show users after April a simpler licence length screen

--- server/routes/buy/licence-short-term-length.js
+++ server/routes/buy/licence-short-term-length.js
@@ -5,17 +5,24 @@ const handlers = {
       pageTitle: 'How long do you want your licence to last?',
       errorMessage: 'Choose a licence length',
       items: {
-          two: {
+          one: {
             text: '1 day',
             name: 'licence_length',
             id: '1-day',
           },
-          three: {
+          two: {
             text: '8 days',
             name: 'licence_length',
             id: '8-days (These licences are valid for 8 consecutive days)',
             value: '8-days',
             selectedText: '8-day licences are valid for 8 consecutive days',
+          },
+          three: {
+            text: '12 months',
+            name: 'licence_length',
+            id: '365-days',
+            value: '365-days',
+            //selectedText: 'A 12-month licence is not available for the start date of you selected. We will amend the date to 1 April 2017',
           },
       }
     })
