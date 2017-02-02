4df75508d21403f8a52196f8f723d641a6b44237
Cathy <Cathy@Steves-MacBook-Pro-2.local>
2017-02-02 15:41:57 +0000

    Add new page url

--- server/routes/start/buy.js
+++ server/routes/start/buy.js
@@ -1,12 +1,12 @@
 const handlers = {
   get: function (request, reply) {
-    return reply.view('buy.html')
+    return reply.view('get.html')
   }
 }
 
 module.exports = [{
   method: 'GET',
-  path: '/start/buy.html',
+  path: '/start/get.html',
   config: {
     handler: handlers.get
   }
