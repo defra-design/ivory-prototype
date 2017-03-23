7b0be535df952d22c799420abef4faf315a5ccb4
Cathy <Cathy@Steves-MacBook-Pro-2.local>
2017-03-23 16:08:56 +0000

    Add content and flow for catch returns

--- server/routes/catch-return/old-licence-number.js
+++ server/routes/catch-return/old-licence-number.js
@@ -2,7 +2,8 @@ const handlers = {
   get: function (request, reply) {
     return reply.view('old-licence-number', {
       pageTitle: 'Enter your licence details',
-      errorMessage: 'Enter your licence details',
+      errorMessage: 'Enter your licence number',
+      errorMessageTwo: 'Enter your postcode',
     })
   },
   post: function (request, reply) {
