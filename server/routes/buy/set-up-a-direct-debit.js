c074a4004a43e9f63a8c6b1a8f28e94cab65b9f6
cathydutton <info@cathydutton.co.uk>
2017-08-09 15:13:34 +0100

    Amend DD content

--- server/routes/buy/set-up-a-direct-debit.js
+++ server/routes/buy/set-up-a-direct-debit.js
@@ -4,9 +4,9 @@ const handlers = {
 
     return reply.view('set-up-a-direct-debit', {
       pageTitle: 'Set up a Direct Debit',
-      errorMessage: 'Enter the name of the account holder',
-      errorMessageTwo: 'Enter the bank/building society account number',
-      errorMessageThree: 'Enter the payment reference',
+      errorMessage: 'Enter the account-holder’s name',
+      errorMessageTwo: 'Enter the bank or building society account number',
+      errorMessageThree: 'Enter your payment reference',
     })
   },
   post: function (request, reply) {
