89829bce5ef1d5fdceef751a61039ca9c6200e59
Liam McMurray <liammcmurray@gmail.com>
2018-05-25 13:56:00 +0100

    contact page tweaks

--- server/routes/buy/contact.js
+++ server/routes/buy/contact.js
@@ -6,7 +6,7 @@ const handlers = {
 
     return reply.view('contact', {
       pageTitle: 'How can we send you your licence details?',
-      errorMessage: 'Tell us how you would like to receive your licence details?',
+      errorMessage: 'You need to provide a valid email address to receive updates',
       email: request.session.email,
       mobile: request.session.mobile,
       is365Contact: request.session.is365Contact,
