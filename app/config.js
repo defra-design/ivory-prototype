a6b11d5ed5b5ad0ece4df76df405c469d0824fa3
Nic Price <nicprice@Nics-MacBook-Pro.local>
2018-10-26 16:45:14 +0100

    Changed service name in config

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Register your favourite Disney character',
+  serviceName: 'Ivory prototypes',
 
   // Default port that prototype runs on
   port: '3000',
