4310b97a87d88fc17613cf75be7c8490d1b26f6f
Nic Price <nic.price@peoplethinking.co.uk>
2019-08-01 18:58:19 +0100

    latest

--- public/javascripts/toc.js
+++ public/javascripts/toc.js
@@ -1,26 +1,26 @@
 
 
   var ToC =
-    "<nav role='navigation' class='table-of-contents'>" +
-      "<h2 class='defra-toc'>Contents</h2>" +
-      "<ol class='govuk-list defra-toc-ol'>";
+    '<nav role="navigation" class="table-of-contents">' +
+      '<h2 class="defra-toc">Contents</h2>' +
+      '<ol class="govuk-list defra-toc-ol">';
 
   var newLine, el, title, link;
 
-  $("h2").each(function() {
+  $('h2').each(function() {
 
     el = $(this);
     title = el.text();
-    link = "#" + el.attr("id");
+    link = '#' + el.attr('id');
 
-    if ( link != "#undefined" ){
+    if ( link != '#undefined' ){
 
     newLine =
-      "<li class='gem-c-contents-list__list-item gem-c-contents-list__list-item--dashed'>" +
-        "<a href='" + link + "'>" +
+      '<li class="gem-c-contents-list__list-item gem-c-contents-list__list-item--dashed">' +
+        '<a href="' + link + '">' +
           title +
-        "</a>" +
-      "</li>";
+        '</a>' +
+      '</li>';
 
     ToC += newLine;
   }
@@ -28,8 +28,8 @@
   });
 
   ToC +=
-     "</ul>" +
-    "</nav>";
+     '</ul>' +
+    '</nav>';
 
 
-  $("#toc").html(ToC);
+  $('#toc').html(ToC);
