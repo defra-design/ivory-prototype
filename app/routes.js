fdb9a643d786f8c0995bfedf1e1d90e85e485a53
Nic Price <nic.price@peoplethinking.co.uk>
2019-08-30 09:26:02 +0100

    v7

--- app/routes.js
+++ app/routes.js
@@ -11,5 +11,6 @@ router.use('/v04', require('./v04/routes/routes'))
 router.use('/v04.2', require('./v04.2/routes/routes'))
 router.use('/v05', require('./v05/routes/routes'))
 router.use('/v06', require('./v06/routes/routes'))
+router.use('/v07', require('./v07/routes/routes'))
 
 module.exports = router
