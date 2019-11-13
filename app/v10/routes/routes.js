a6c1ac3cda1529819bfbb7551181617a1697fa57
David Harris <davidharriswork@gmail.com>
2019-11-13 16:36:36 +0000

    improve photos

--- app/v10/routes/routes.js
+++ app/v10/routes/routes.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
-
-// Set the views with a relative path (haven't yet found a better way of doing this yet)
-const viewsFolder = __dirname + '/../views/'
+const path = require('path')
+const viewsFolder = path.join(__dirname, '/../views/') // Set the views with a relative path (haven't yet found a better way of doing this yet)
 
 // Route index page
 router.get('/', function (req, res) {
@@ -12,5 +11,6 @@ router.get('/', function (req, res) {
 router.use('/public', require('./routes-public.js'))
 router.use('/apha', require('./routes-apha.js'))
 router.use('/experiments', require('./routes-experiments.js'))
+router.use('/', require('./routes-shared.js'))
 
 module.exports = router
