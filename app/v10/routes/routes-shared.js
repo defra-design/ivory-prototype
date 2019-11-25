602a9cc1ff3c8c44589bf5d729f0b3338838c1c8
David Harris <davidharriswork@gmail.com>
2019-11-25 17:02:11 +0000

    photos root directory fix 2

--- app/v10/routes/routes-shared.js
+++ app/v10/routes/routes-shared.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const rootAppDirectory = require('../../config').rootAppDirectory // Used when a full path is required, e.g. '/Users/dave/Documents/NODE/projects/DEFRA/ivory-prototype/app' or '/app/app' on Heroku
-const version = __dirname.match(/app\/(.*?)\/routes/)[1] // Gets the version, e.g. v10
+const version = __dirname.match(/app\/(.[^\/]*?)\/routes/)[1]// Gets the version, e.g. v10 (ensure this handles Heroku's __direname being /app/app/vXX/routes)
 
 /**
  * PHOTOS
